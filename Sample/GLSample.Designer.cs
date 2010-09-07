bc364d8a83b2eaff3ac6466011b3b6ba1a49a256
breaker.kgd <breaker.kgd@5ac221ee-52f3-08ba-6652-1ed71a1f7d95>
2010-09-07 14:44:19 +0000

    Добавил контрол на форму, пока в дизайнере не работает.

--- Sample/GLSample.Designer.cs
+++ Sample/GLSample.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace Sample
 {
-	partial class Sample1
+	partial class GLSample
 	{
-		/// <summary>
+		/// <summary> 
 		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
+		/// <summary> 
 		/// Clean up any resources being used.
 		/// </summary>
 		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,42 +20,26 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+		#region Component Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.glSample1 = new Sample.GLSample();
 			this.SuspendLayout();
 			// 
-			// glSample1
-			// 
-			this.glSample1.BackColor = System.Drawing.Color.Black;
-			this.glSample1.Location = new System.Drawing.Point(21, 33);
-			this.glSample1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.glSample1.Name = "glSample1";
-			this.glSample1.Size = new System.Drawing.Size(541, 240);
-			this.glSample1.TabIndex = 0;
-			this.glSample1.VSync = false;
-			// 
-			// Sample1
+			// GLSample
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(614, 344);
-			this.Controls.Add(this.glSample1);
-			this.Name = "Sample1";
-			this.Text = "Form1";
+			this.Name = "GLSample";
+			this.Load += new System.EventHandler(this.GLSample_Load);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private GLSample glSample1;
 	}
 }
-
---
+++ Sample/GLSample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using GLGDIPlus;
+
+namespace Sample
+{
+	public partial class GLSample : GLControl
+	{
+		// ============================================================
+		bool mIsLoaded = false;
+
+		GLGraphics mGraphics = new GLGraphics();
+		// ============================================================
+		public GLSample()
+		{
+			InitializeComponent();
+		}
+		// ============================================================
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			if (!mIsLoaded)		// чтобы отследить, что OpenGL контекст еще не создан
+				return;
+
+			MakeCurrent();
+
+			GLGraphics g = mGraphics;
+			g.Reset();
+			g.Clear();
+
+			SwapBuffers();
+		}
+		// ============================================================
+		private void GLSample_Load(object sender, EventArgs e)
+		{
+			mIsLoaded = true;	// OpenGL контекст уже должен быть создан
+
+			if (this.IsHandleCreated)
+			{
+				mGraphics.Init();
+				mGraphics.Clear();
+				mGraphics.Reset();
+				mGraphics.SetLinearFiltering(false);	// чтобы картинки были четкими (но при скейлинге они будут пикселизированы)
+				mGraphics.SetClearColor(SystemColors.ActiveCaption);
+			}
+		}
+		// ============================================================
+	}
+}

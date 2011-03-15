1b826ad2a08df46522462c75ad9976ae4bbbb06f
breaker.kgd <breaker.kgd@5ac221ee-52f3-08ba-6652-1ed71a1f7d95>
2011-03-15 21:35:21 +0000

    no changes

--- GLGDIPlus/GLMultiImage.cs
+++ GLGDIPlus/GLMultiImage.cs
@@ -105,27 +105,29 @@ namespace GLGDIPlus
 		/// <param name="path">Image path.</param>
 		public void FromBitmap(Bitmap src)
 		{
-			bitmap = src;
+			{
+				bitmap = src;
 
-			// Generate texture
-			GL.GenTextures(1, out TextureIndex);
-			GL.BindTexture(TextureTarget.Texture2D, TextureIndex);
+				// Generate texture
+				GL.GenTextures(1, out TextureIndex);
+				GL.BindTexture(TextureTarget.Texture2D, TextureIndex);
 
-			// Store texture size
-			Width = bitmap.Width;
-			Height = bitmap.Height;
+				// Store texture size
+				Width = bitmap.Width;
+				Height = bitmap.Height;
 
-			BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-				ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+				BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+					ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
-			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
-				OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+				GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+					OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
 
-			bitmap.UnlockBits(data);
+				bitmap.UnlockBits(data);
 
-			// Setup filtering
-			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+				// Setup filtering
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+			}
 		}
 
 
